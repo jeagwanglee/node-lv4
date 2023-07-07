eac579e7560589ce48a44a91cd4d081624bdb6d1
jegwlee <jegwlee@gmail.com>
2023-07-07 10:46:16 +0900

    Feat: 댓글 수정  API 구현

--- routes/comments.route.js
+++ routes/comments.route.js
@@ -30,6 +30,7 @@ router.get('/:postId', async (req, res) => {
 
   try {
     const post = await Posts.findOne({ where: { postId } });
+
     if (!post) {
       return res.status(404).json({ errorMessage: '게시글 조회에 실패하였습니다.' });
     }
@@ -38,6 +39,42 @@ router.get('/:postId', async (req, res) => {
     res.status(200).json({ comments });
   } catch (error) {
     res.status(400).json({ errorMessage: '댓글 조회에 실패하였습니다.' });
+  }
+});
+
+router.put('/:postId/:commentId', authMiddleware, async (req, res) => {
+  const { userId } = res.locals.user;
+  const { postId, commentId } = req.params;
+  const { comment } = req.body;
+
+  try {
+    const post = await Posts.findOne({ where: { postId } });
+    const targetComment = await Comments.findOne({ where: { commentId } });
+
+    if (!comment) {
+      return res.status(412).json({ errorMessage: '데이터 형식이 올바르지 않습니다.' });
+    }
+
+    if (!post) {
+      return res.status(404).json({ errorMessage: '게시글 조회에 실패하였습니다.' });
+    }
+
+    if (!targetComment) {
+      return res.status(404).json({ errorMessage: '댓글 조회에 실패하였습니다.' });
+    }
+
+    if (userId !== targetComment.UserId) {
+      return res.status(403).json({ errorMessage: '댓글 수정 권한이 없습니다.' });
+    }
+  } catch (error) {
+    res.status(400).json({ errorMessage: '댓글 수정에 실패하였습니다.' });
+  }
+
+  try {
+    await Comments.update({ comment }, { where: { commentId } });
+    res.status(200).json({ message: '댓글을 수정하였습니다.' });
+  } catch (error) {
+    res.status(400).json({ errorMessage: '댓글 수정이 정상적으로 처리되지 않았습니다.' });
   }
 });
 

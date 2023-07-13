3a8e1b01ceb788681f7584eb9219e8425467b638
jegwlee <jegwlee@gmail.com>
2023-07-13 16:48:07 +0900

    Remove: 사용하지 않는 코드 삭제

--- repositories/users.repository.js
+++ repositories/users.repository.js
@@ -1,19 +1,6 @@
 const { Users } = require('../models');
 
 class UsersRepository {
-  findOneUser = async (nickname) => {
-    const existUser = await Users.findOne({
-      where: {
-        nickname,
-      },
-    });
-    return existUser;
-  };
-
-  createUser = async (nickname, password) => {
-    await Users.create({ nickname, password });
-  };
-
   findOrCreateUser = async (nickname, password) => {
     const [user, created] = await Users.findOrCreate({
       where: { nickname },
